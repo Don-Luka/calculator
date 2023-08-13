c52510a47a8679aefc20676561111e4e5fbf1908
Don-Luka <140162151+Don-Luka@users.noreply.github.com>
2023-08-13 10:19:28 +0200

    Changed prompt for sum and product

--- kalkulator.py
+++ kalkulator.py
@@ -25,13 +25,16 @@ def main():
     dzialanie = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
     
     if dzialanie == '1':
-        print("Możesz podać 2 składniki lub więcej.\n Po zakończeniu wprowadzania naciśnij Enter")
+        print("Możesz podać 2 składniki lub więcej.")
         wynik = 0
         skladniki = []
         i = 0
         while True:
             i += 1
-            liczba = input(f'Podaj składnik nr {i} lub naciśnij Enter: ')
+            if i <= 2:
+                liczba = input(f'Podaj składnik nr {i}: ')
+            else:
+                liczba = input(f'Podaj składnik nr {i} lub naciśnij Enter: ')
             if liczba != '':
                 if liczba.isalpha():
                     logging.warning("Podana wartość nie jest liczbą!")
@@ -76,15 +79,17 @@ def main():
         logging.info(f'Wynik dodawania to: {wynik}.')
         # time.sleep(5)
         
-    
     elif dzialanie == '3':
-        print("Możesz podać 2 czynniki lub więcej.\n Po zakończeniu wprowadzania naciśnij Enter")
+        print("Możesz podać 2 czynniki lub więcej.")
         wynik = 1
         czynniki = []
         i = 0
         while True:
             i += 1
-            liczba = input(f'Podaj czynniki nr {i} lub naciśnij Enter: ')
+            if i <= 2:
+                liczba = input(f'Podaj czynnik nr {i}: ')
+            else:
+                liczba = input(f'Podaj czynnik nr {i} lub naciśnij Enter: ')
             if liczba != '':
                 if liczba.isalpha():
                     logging.warning("Podana wartość nie jest liczbą!")
